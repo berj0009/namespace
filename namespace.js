bd6c0503e080ca9ee07ea62d1e4d7b22abdccac1
Rachad <43101690+berj0009@users.noreply.github.com>
2018-10-25 22:38:34 -0400

    Add files via upload

---
+++ namespace.js
@@ -0,0 +1,30 @@
+var BERJ0009 = {
+	init: function () 
+	{
+		let newDiv = document.createElement("div");
+		newDiv.className("box");
+		newDiv.textContent("berj0009");
+		document.getElementById("boxes").appendChild(newDiv);
+
+
+		newDiv.addEventListener("mouseover", function () {
+			newDiv.classList.toggle("highlight");
+
+		});
+
+		newDiv.addEventListener("mouseout", function () {
+			newDiv.classList.toggle("highlight");
+			newDiv.removeAttribute("style");
+
+		});
+
+		newDiv.addEventListener("click", function () {
+			newDiv.style.borderColor = "blue";
+			newDiv.style.backgroundColor = "yellow";
+
+		});
+
+	}
+
+
+};
